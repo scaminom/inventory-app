dfbe7c3dfcb939f4815eb477135317e45e6071e2
vKail <ajurado5411@uta.edu.ec>
2023-12-06 14:42:06 -0500

    Created table company

--- inventory-back/inventory-api/db/schema.rb
+++ inventory-back/inventory-api/db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_06_042333) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_06_194042) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "companies", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "laboratories", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+  end
 
   create_table "roles", force: :cascade do |t|
     t.string "name"
