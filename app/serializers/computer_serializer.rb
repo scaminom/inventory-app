4c74a25c7a4a90343954d47c9c0918d84344518f
scaminom <jcamino7758@uta.edu.ec>
2023-12-13 23:53:03 -0500

    Fixing computer serializer

--- app/serializers/computer_serializer.rb
+++ app/serializers/computer_serializer.rb
@@ -1,6 +1,7 @@
 class ComputerSerializer < Panko::Serializer
   attributes  :id,
-              :name
+              :name,
+              :description
 
   has_many :peripherals, each_serializer: PeripheralSerializer
 end
