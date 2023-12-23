6743cf8ebdb92f71897179aff181b9e322ce6874
JMSebas <jmoreira8505@uta.edu.ec>
2023-12-22 21:47:04 -0500

    Change routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :activities
+  
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations'
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
       resources :custodians
       resources :assets
       resources :suggestions
+      resources :activities
     end
   end
 
