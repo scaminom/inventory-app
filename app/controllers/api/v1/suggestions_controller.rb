b82e2246f39eb952ef5b80df348962a529e05e9b
JMSebas <jmoreira8505@uta.edu.ec>
2023-12-22 19:40:54 -0500

    Change suggestions_controller

--- app/controllers/api/v1/suggestions_controller.rb
+++ app/controllers/api/v1/suggestions_controller.rb
@@ -1,51 +1,70 @@
+module Api
+  module V1
 class SuggestionsController < ApplicationController
   before_action :set_suggestion, only: %i[ show update destroy ]
 
-  # GET /suggestions
+
   def index
-    @suggestions = Suggestion.all
+    suggestions = Suggestion.all
 
-    render json: @suggestions
+    render json: Panko::ArraySerializer.new(
+      suggestions, each_serializer: SuggestionSerializer
+    ).to_json
   end
 
-  # GET /suggestions/1
+  
   def show
-    render json: @suggestion
+    render json: { suggestion: suggestion_serializer(@suggestion) }
   end
 
-  # POST /suggestions
   def create
     @suggestion = Suggestion.new(suggestion_params)
 
     if @suggestion.save
-      render json: @suggestion, status: :created, location: @suggestion
+      render json: { suggestion: suggestion_serializer(@suggestion) }, status: :accepted
     else
-      render json: @suggestion.errors, status: :unprocessable_entity
+      render json: { errors: @suggestion.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /suggestions/1
   def update
     if @suggestion.update(suggestion_params)
-      render json: @suggestion
+      render json: { suggestion: suggestion_serializer(@suggestion) }
     else
-      render json: @suggestion.errors, status: :unprocessable_entity
+      render json: { errors: @suggestion.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
-  # DELETE /suggestions/1
   def destroy
-    @suggestion.destroy!
+    if @suggestion.destroy
+      render json: { message: 'Suggestion deleted successfully' }
+    else
+      render json: { errors: @suggestion.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_suggestion
-      @suggestion = Suggestion.find(params[:id])
-    end
+  def set_suggestion
+    @suggestion = Suggestion.find(params[:id])
+  end
 
-    # Only allow a list of trusted parameters through.
-    def suggestion_params
-      params.require(:suggestion).permit(:title, :description)
+  def suggestion_params
+    params.require(:suggestion).permit(:title, :description)
+  end
+
+  def suggestion_serializer(suggestion)
+    Rails.cache.fetch([cache_key(suggestion), I18n.locale]) do
+      SuggestionSerializer.new.serialize(suggestion)
+    rescue StandardError => e
+      Rails.logger.error "Error serializing suggestion #{suggestion.id}: #{e.message}"
+      {}.as_json
     end
+  end
+
+  def cache_key(suggestion)
+    "suggestions/#{suggestion.id}"
+  end
+  
+  end
+  end
 end
