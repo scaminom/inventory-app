57622909afc976f9751145fa4387a0babff02bfb
JMSebas <jmoreira8505@uta.edu.ec>
2023-12-22 19:56:08 -0500

    Change suggestions controller and Add whitelisted

--- app/controllers/api/v1/suggestions_controller.rb
+++ app/controllers/api/v1/suggestions_controller.rb
@@ -1,70 +1,70 @@
 module Api
   module V1
-class SuggestionsController < ApplicationController
-  before_action :set_suggestion, only: %i[ show update destroy ]
+    class SuggestionsController < ApplicationController
+     before_action :set_suggestion, only: %i[ show update destroy ]
 
 
-  def index
-    suggestions = Suggestion.all
+      def index
+        suggestions = Suggestion.all
 
-    render json: Panko::ArraySerializer.new(
-      suggestions, each_serializer: SuggestionSerializer
-    ).to_json
-  end
+        render json: Panko::ArraySerializer.new(
+        suggestions, each_serializer: SuggestionSerializer
+        ).to_json
+      end
 
   
-  def show
-    render json: { suggestion: suggestion_serializer(@suggestion) }
-  end
+      def show
+        render json: { suggestion: suggestion_serializer(@suggestion) }
+      end
 
-  def create
-    @suggestion = Suggestion.new(suggestion_params)
+      def create
+        @suggestion = Suggestion.new(suggestion_params)
 
-    if @suggestion.save
-      render json: { suggestion: suggestion_serializer(@suggestion) }, status: :accepted
-    else
-      render json: { errors: @suggestion.errors.full_messages }, status: :unprocessable_entity
+        if @suggestion.save
+          render json: { suggestion: suggestion_serializer(@suggestion) }, status: :accepted
+       else
+          render json: { errors: @suggestion.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
+
+      def update
+        if @suggestion.update(suggestion_params)
+          render json: { suggestion: suggestion_serializer(@suggestion) }
+        else
+          render json: { errors: @suggestion.errors.full_messages }, status: :unprocessable_entity
+       end
+      end
+
+      def destroy
+        if @suggestion.destroy
+          render json: { message: 'Suggestion deleted successfully' }
+        else
+         render json: { errors: @suggestion.errors.full_messages }, status: :unprocessable_entity
+        end
+     end
+
+      private
+      def set_suggestion
+       @suggestion = Suggestion.find(params[:id])
+      end
+
+      def suggestion_params
+        params.require(:suggestion).permit(:title, :description)
+      end
+
+      def suggestion_serializer(suggestion)
+        Rails.cache.fetch([cache_key(suggestion), I18n.locale]) do
+          SuggestionSerializer.new.serialize(suggestion)
+        rescue StandardError => e
+          Rails.logger.error "Error serializing suggestion #{suggestion.id}: #{e.message}"
+          {}.as_json
+        end
+      end
+
+      def cache_key(suggestion)
+       "suggestions/#{suggestion.id}"
+      end
+
     end
-  end
-
-  def update
-    if @suggestion.update(suggestion_params)
-      render json: { suggestion: suggestion_serializer(@suggestion) }
-    else
-      render json: { errors: @suggestion.errors.full_messages }, status: :unprocessable_entity
-    end
-  end
-
-  def destroy
-    if @suggestion.destroy
-      render json: { message: 'Suggestion deleted successfully' }
-    else
-      render json: { errors: @suggestion.errors.full_messages }, status: :unprocessable_entity
-    end
-  end
-
-  private
-  def set_suggestion
-    @suggestion = Suggestion.find(params[:id])
-  end
-
-  def suggestion_params
-    params.require(:suggestion).permit(:title, :description)
-  end
-
-  def suggestion_serializer(suggestion)
-    Rails.cache.fetch([cache_key(suggestion), I18n.locale]) do
-      SuggestionSerializer.new.serialize(suggestion)
-    rescue StandardError => e
-      Rails.logger.error "Error serializing suggestion #{suggestion.id}: #{e.message}"
-      {}.as_json
-    end
-  end
-
-  def cache_key(suggestion)
-    "suggestions/#{suggestion.id}"
-  end
-  
-  end
   end
 end
--- app/models/suggestion.rb
+++ app/models/suggestion.rb
@@ -1,3 +1,8 @@
 class Suggestion < ApplicationRecord
     belongs_to :asset
+    WHITELISTED_ATTRIBUTES = %i[
+     title
+     description
+     asset_id
+    ].freeze
 end
